2ecb08e2744e1bbd86bb9fe8e746cc49b2146b1c
vivi <pollivivian@gmail.com>
2025-03-09 09:23:41 -0300

    show personal agent

--- frontend/src/web3authContext.js
+++ frontend/src/web3authContext.js
@@ -1,9 +1,7 @@
 import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 
-const clientId =
-  import.meta.env.VITE_WEB3AUTH_CLIENT_ID ||
-  "BCYyGA2ohL6R7jt1X37YDx52xiAXOnbnXQZsROAyF0YQ2G8Uf6RFA78ngA0Knn1CZMWpC75Kp8n7AzKO2_9fsww";
+const clientId = import.meta.env.VITE_WEB3AUTH_CLIENT_ID;
 
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
